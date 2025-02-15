fc4eca504ed60a0acd645fedfb87fe534c0dbf3a
Maksym Pecheniuk <maksym.pecheniuk@zorallabs.com>
2025-02-15 21:10:01 +0200

    Revert "check projects access"
    This reverts commit 5925b3c5678bc4e0f7a563de2c24af07290fe6f9.

--- pybossa/auth/helpingmaterial.py
+++ pybossa/auth/helpingmaterial.py
@@ -40,7 +40,15 @@ class HelpingMaterialAuth(object):
         return self._is_admin_or_owner(user, project)
 
     def _read(self, user, helpingmaterial=None, project_id=None):
-        return False
+        if helpingmaterial or project_id:
+            project = self._get_project(helpingmaterial, project_id)
+            if project:
+                return (project.published or self._is_admin_or_owner(user, project))
+            if user.is_anonymous or (helpingmaterial is None and project_id is None):
+                return False
+            return self._is_admin_or_owner(user, project)
+        else:
+            return True
 
     def _update(self, user, helpingmaterial, project_id=None):
         project = self._get_project(helpingmaterial, project_id)
